1a0981925469737d13836712f3705f27692ea050
Dorian Bayart <dorianbayart@users.noreply.github.com>
2019-10-04 17:35:40 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,7 @@ scrollToElementId(getAnchor());
 function scrollToElementId(elementId) {
     console.log(elementId);
   var element = document.getElementById(elementId);
+    console.log(element);
   if(element) element.scrollIntoView({behavior: "smooth"});
 }
 
